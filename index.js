34c465b55bca2d4e5cff6fdb593ee9bf88a30458
AARTHI-m32 <aarthi.m2022cse@sece.ac.in>
2024-05-25 19:52:06 +0530

    update

--- index.js
+++ index.js
@@ -54,34 +54,33 @@ app.post('/add-user',async function(request,response){
     }
 })
 
-app.post('/req-user',async function(request,response){
-    try{
-        const {username,password}=request.body
-        const user=await Register.findOne({username,password})
-
-        if(user){
-            response.status(200).json({
-                "status":"success",
-                "message":"Valid user"
-            })
-        }
-        else{
-            response.status(401).json({
-                "status":"failure",
-                "message":"Invalid user"
-            })
-        }
-    }
-    
-    catch (error) {
-        console.error('Error fetching users:', error);
-        response.status(500).json({
-          status: 'failure',
-          message: 'Failed to fetch users',
-          error: error.message
-        })
+app.post('/req-user', async (request, response) => {
+    try {
+      const { username, password } = request.body;
+      const user = await Register.findOne({ username, password });
+  
+      if (user) {
+        response.status(200).json({
+          status: "success",
+          message: "Valid user",
+          _id: user._id
+        });
+      } else {
+        response.status(401).json({
+          status: "failure",
+          message: "Invalid user"
+        });
       }
-})
+    } catch (error) {
+      console.error('Error fetching users:', error);
+      response.status(500).json({
+        status: 'failure',
+        message: 'Failed to fetch users',
+        error: error.message
+      });
+    }
+  });
+  
 
 app.post('/add-donated',async function (request,response){
     
